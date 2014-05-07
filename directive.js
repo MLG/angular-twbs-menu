338320a4b31d5b58f227bf330df3b4a3070d66c3
Eugene OZ <normandiggs@gmail.com>
2014-05-07 20:58:06 +0400

    v0.0.6

--- directive.js
+++ directive.js
@@ -1,15 +1,16 @@
 angular.module('mlgTWBSMenu', [])
   .directive('mlgTwbsMenu', function () {
     return {
-      restrict: 'AE',
-      transclude: true,
+      restrict:    'AE',
+      transclude:  true,
       templateUrl: 'views/menu.html', // copy menu.html of this repository to your app/views/menu.html
-      scope: {
+      scope:       {
         authPath: '@',
         initPath: '@',
-        appName: '@'
+        appName:  '@'
       },
-      controller: function ($scope, Auth) {
+      controller:  function ($scope, Auth) {
+        $scope.Auth = Auth;
         $scope.init = function () {
           Auth.authenticate($scope.initPath, $scope.authPath);
         };
